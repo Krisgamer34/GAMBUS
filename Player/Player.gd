de290d649544845176198c83b4e903b946c5d6ea
Kristian <krisgamer@live.com>
2023-01-19 08:46:36 +0100

    Part 6 fix

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,7 +8,7 @@ const acceleration = 500
 const FRICTION = 500
 
 onready var animationPlayer = $AnimationPlayer
-
+onready var animationTree =$AnimationTree
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -16,15 +16,15 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
+	
+	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idle/blend_position", input_vector) 
+		animationTree.set("parameters/Run/blend_position", input_vector)
 		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, acceleration*delta)
 		
 	else:
-		animationPlayer.play("IdleRight")
+		
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
